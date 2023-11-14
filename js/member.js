ef0d249a72df4a0ecb89212019fab55b936486fa
dogtata <furadeya000@gmail.com>
2023-11-14 15:34:39 +0800

    備份

---
+++ js/member.js
@@ -0,0 +1,32 @@
+const groupMember = Object.freeze(
+    [
+        {
+            name: '黃絜琳',
+            position: '組長/程式',
+            image: './img/member2.jpg',
+        },
+        {
+            name: '蔡明哲',
+            position: '程式',
+            image: './img/member5.jpg',
+        },
+        {
+            name: '陳映達',
+            position: '企劃',
+            image: './img/member4.jpg',
+        },
+        {
+            name: '吳睿燊',
+            position: '設計',
+            image: './img/member3.jpg',
+        },
+        {
+            name: '賴菱俐',
+            position: '設計',
+            image: './img/member1.jpg',
+        },
+    ]
+);
+
+
+export { groupMember };
